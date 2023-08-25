7fb8cc6c4ec5787f1c6f9bc3cfa5d61a5370040d
nikseyy <68756650+nikseyy@users.noreply.github.com>
2023-08-25 21:03:23 +0200

    Update config.h

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,8 +1,5 @@
-/* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
+static int topbar = 1;                     
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"terminus:size=13"
 };
@@ -13,11 +10,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+
 static unsigned int lines      = 0;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
+
 static const char worddelimiters[] = " ";
